8e70958dfc8350a685628bf7c3bdf0a40b868867
Fabio Santos <fabiojunior96@outlook.com>
2022-11-21 14:34:21 -0300

    modelo dinamico de formulario para criação de usuários e criação de um novo paciente

---
+++ app/ui/src/formSource.js
@@ -0,0 +1,11 @@
+export const userInputs = [
+    {id:1,label:"Usuário", type:"text",placeholder:"jon_snow"},
+    {id:2,label:"Nome", type:"text",placeholder:"Jon Snow"},
+    {id:3,label:"Email", type:"mail",placeholder:"jonsnow@westeros.com"},
+    {id:4,label:"Telefone", type:"text",placeholder:"+55 (25) 9999-9999"},
+    {id:5,label:"Senha", type:"password"},
+    {id:6,label:"Endereço", type:"text", placeholder:"Muralha, portão 2"},
+    {id:6,label:"País", type:"text", placeholder:"Westeros"},
+]
+
+export const patientInputs = []
--- app/ui/src/index.js
+++ app/ui/src/index.js
@@ -10,27 +10,28 @@ import { Login } from './pages/login/Login';
 import { List } from './pages/list/List';
 import { Single } from './pages/single/Single';
 import { New } from './pages/new/New';
+import { userInputs, patientInputs } from './formSource';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-        <Routes>
-            <Route path="/">
-                <Route index element={<Home />}/> 
-                <Route path='login' element={<Login />}/>
-                <Route path='users'>
-                    <Route index element={ <List />}/> 
-                    <Route path=':id' element={ <Single />}/> 
-                    <Route path='new' element={ <New />}/> 
-                </Route> 
-                <Route path='patients'>
-                    <Route index element={ <List />}/> 
-                    <Route path=':id' element={ <Single />}/> 
-                    <Route path='new' element={ <New />}/> 
-                </Route> 
-            </Route>
-        </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<Home />} />
+                    <Route path='login' element={<Login />} />
+                    <Route path='users'>
+                        <Route index element={<List />} />
+                        <Route path=':id' element={<Single />} />
+                        <Route path='new' element={<New inputs={userInputs} title="Adicionar novo usuário" />} />
+                    </Route>
+                    <Route path='patients'>
+                        <Route index element={<List />} />
+                        <Route path=':id' element={<Single />} />
+                        <Route path='new' element={<New inputs={patientInputs} title="Adicionar novo Paciente" />} />
+                    </Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
