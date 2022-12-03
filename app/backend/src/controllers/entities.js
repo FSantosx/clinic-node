6b3a2f6633004394a257bba5a5210643d9a57822
= <fsantos@outlook.com>
2022-12-02 23:30:02 -0300

    CRUD Controller ok !

--- app/backend/src/controllers/entities.js
+++ app/backend/src/controllers/entities.js
@@ -26,39 +26,39 @@ module.exports = class Entities {
         }
     }
 
-    static create(table) {
-        const files = IO.scan(`./db/${table}`)
-        const arr = []
-        files.map(file => {
-            arr.push(IO.jout(`./db/${table}/${file}`))
-        })
-        return arr;;
+    static create(table, payload) {
+        let newid = parseInt((Math.random() * 1000000))
+        const file = `./db/${table}/${newid}.json`
+        var obj = {'id' : newid}
+        for (const [field, value] of Object.entries( payload )) {
+            obj[ field ] = value
+        }      
+        if (IO.jin(file, obj)) {
+            return newid ;;
+        } else {
+            return false;
+        }
     }
 
     static list(table) {
         const files = IO.scan(`./db/${table}`)
         const arr = []
         files.map(file => {
-            arr.push(IO.jout(`./db/${table}/${file}`))
+           arr.push(IO.jout(`./db/${table}/${file}`))
         })
         return arr;;
     }
     
-    static edit(table) {
-        const files = IO.scan(`./db/${table}`)
-        const arr = []
-        files.map(file => {
-            arr.push(IO.jout(`./db/${table}/${file}`))
-        })
+    static edit(table, id) {
+        const file = `./db/${table}/${id}.json`
+        let arr = []
+        arr.push(IO.jout(file))
         return arr;;
     }
 
-    static delete(table) {
-        const files = IO.scan(`./db/${table}`)
-        const arr = []
-        files.map(file => {
-            arr.push(IO.jout(`./db/${table}/${file}`))
-        })
-        return arr;;
+    static delete(table, id) {
+        const file = `./db/${table}/${id}.json`        
+        IO.rm(file)
+        return 'true';;                 
     }
 }
